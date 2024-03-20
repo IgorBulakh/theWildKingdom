88e58ecf3616e042051dd866ce067e620f7692f2
IgorBulakh <bulach.igor@gmail.com>
2024-03-20 19:38:01 +0200

    last section

--- src/js/files/gallery.js
+++ src/js/files/gallery.js
@@ -1,4 +1,3 @@
-
 /*
 Документація по роботі у шаблоні: https://www.lightgalleryjs.com/docs/
 Документація плагіна: https://www.lightgalleryjs.com/docs/
@@ -11,7 +10,7 @@ import { isMobile, FLS } from "./functions.js";
 import { flsModules } from "./modules.js";
 
 // Підключення базового набору функціоналу
-import lightGallery from 'lightgallery';
+import lightGallery from "lightgallery";
 
 // Плагіни
 // lgZoom, lgAutoplay, lgComment, lgFullscreen, lgHash, lgPager, lgRotate, lgShare, lgThumbnail, lgVideo, lgMediumZoom
@@ -19,7 +18,7 @@ import lightGallery from 'lightgallery';
 //import lgZoom from 'lightgallery/plugins/zoom/lg-zoom.min.js'
 
 // Базові стилі
-import '@scss/libs/gallery/lightgallery.scss';
+import "@scss/libs/gallery/lightgallery.scss";
 // Стилі доповнень
 // import '@scss/libs/gallery/lg-thumbnail.scss';
 // import '@scss/libs/gallery/lg-video.scss';
@@ -37,24 +36,20 @@ import '@scss/libs/gallery/lightgallery.scss';
 // import '@scss/libs/gallery/lightgallery-bundle.scss';
 
 // Запуск
-const galleries = document.querySelectorAll('[data-gallery]');
+const galleries = document.querySelectorAll("[data-gallery]");
 if (galleries.length) {
-	let galleyItems = [];
-	galleries.forEach(gallery => {
-		galleyItems.push({
-			gallery,
-			galleryClass: lightGallery(gallery, {
-				// plugins: [lgZoom, lgThumbnail],
-				licenseKey: '7EC452A9-0CFD441C-BD984C7C-17C8456E',
-				speed: 500,
-			})
-		})
-	});
-	// Додаємо в об'єкт модулів
-	flsModules.gallery = galleyItems;
+  let galleyItems = [];
+  galleries.forEach((gallery) => {
+    galleyItems.push({
+      gallery,
+      galleryClass: lightGallery(gallery, {
+        // plugins: [lgZoom, lgThumbnail],
+        licenseKey: "7EC452A9-0CFD441C-BD984C7C-17C8456E",
+        speed: 500,
+        selector: ".gallery__picture",
+      }),
+    });
+  });
+  // Додаємо в об'єкт модулів
+  flsModules.gallery = galleyItems;
 }
-
-
-
-
-
